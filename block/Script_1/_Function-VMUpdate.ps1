10d9bce919dda255002392da55b4d4a78fdb7f01
AntoineBourdarel <86509124+AntoineBourdarel@users.noreply.github.com>
2021-07-06 22:37:32 +0200

    Create _Function-VMUpdate.ps1

---
+++ block/Script_1/_Function-VMUpdate.ps1
@@ -0,0 +1,10 @@
+function VM_update {
+    param ([string]$vm_name,[string]$RG_name,[string]$Location_name,[string]$VNet_name,[string]$subnet_name, [string]$NSG_name)
+
+    #Update le "Standard_B1s" à la création de la VM
+    Stop-AzVM -ResourceGroupName $RG_name -Name $vm_name -Force
+    $vm = Get-AzVM -ResourceGroupName $RG_name -VMName $vm_name
+    $vm.HardwareProfile.VmSize = "Standard_B1s"
+    Update-AzVM -VM $vm -ResourceGroupName $RG_name
+    Start-AzVM -ResourceGroupName $RG_name  -Name $vm.name
+}
