9bb4aba97d90c9ff63c9228b916671cae5ae69b6
AntoineBourdarel <86509124+AntoineBourdarel@users.noreply.github.com>
2021-07-06 22:40:05 +0200

    Delete _VM-Update.ps1
    rm

--- block/Script_1/_VM-Update.ps1
+++
@@ -1,5 +0,0 @@
-Stop-AzVM -ResourceGroupName $RG_name -Name $vm_name -Force
-$vm = Get-AzVM -ResourceGroupName $RG_name -VMName $vm_name
-$vm.HardwareProfile.VmSize = "Standard_B1s"
-Update-AzVM -VM $vm -ResourceGroupName $RG_name
-Start-AzVM -ResourceGroupName $RG_name  -Name $vm.name
