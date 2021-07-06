a87d3e2053d7c4c602f85de069088f6f29d30272
AntoineBourdarel <86509124+AntoineBourdarel@users.noreply.github.com>
2021-07-06 22:39:47 +0200

    Update _NSG-rule.ps1

--- block/Script_1/_NSG-rule.ps1
+++ block/Script_1/_NSG-rule.ps1
@@ -1,21 +1,24 @@
+############################################################ NSG ###########################################################
 #Ajout d'une règle entrante RDP au NSG afin de pouvoir prendre la main sur la machine
 
-$nsg = (Get-AzNetworkSecurityGroup).SecurityRules
-$rules = $nsg.DestinationPortRange
+$get_nsg_rules = (Get-AzNetworkSecurityGroup).SecurityRules
+$get_rules_ports = $get_nsg_rules.DestinationPortRange
 
-foreach ($rule in $rules){
+foreach ($rule in $get_rules_ports){
     
-    if ($rules -eq "3389"){
+    if ($get_rules_ports -eq "3389"){
+        Write-Output " === Règle Firewall === "
+        Write-Output "La règle RDP existe déjà."
         break
     }
     else {
-        $nsg = Get-AzNetworkSecurityGroup -Name $NSG_name -ResourceGroupName $RG_name
+        $get_nsg_rules = Get-AzNetworkSecurityGroup -Name $NSG_name -ResourceGroupName $RG_name
         #Add the inbound security rule.
-        $nsg | Add-AzNetworkSecurityRuleConfig -Name "AllowRDPPort" -Description "Allow RDP port" -Access Allow `
+        $get_nsg_rules | Add-AzNetworkSecurityRuleConfig -Name "AllowRDPPort" -Description "Allow RDP port" -Access Allow `
         -Protocol * -Direction Inbound -Priority 3901 -SourceAddressPrefix "*" -SourcePortRange * `
         -DestinationAddressPrefix * -DestinationPortRange 3389
         # Update the NSG.
-        $nsg | Set-AzNetworkSecurityGroup
-        break 
+        $get_nsg_rules | Set-AzNetworkSecurityGroup
+        break
     }
 }
