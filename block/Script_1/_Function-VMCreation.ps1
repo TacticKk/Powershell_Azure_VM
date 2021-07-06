5e043a62a8b95718ecf1cba09c482ab3aa489899
AntoineBourdarel <86509124+AntoineBourdarel@users.noreply.github.com>
2021-07-06 22:37:44 +0200

    Update _Function-VMCreation.ps1

--- block/Script_1/_Function-VMCreation.ps1
+++ block/Script_1/_Function-VMCreation.ps1
@@ -1,18 +1,10 @@
 function VM_creation {
     param ([string]$vm_name,[string]$RG_name,[string]$Location_name,[string]$VNet_name,[string]$subnet_name, [string]$NSG_name)
 
-    <#$Username = Read "Nom pour le compte de base de la VM ?"
-    $Password = 'Password de base pour la VM ?' #Trouver le moyen de cacher le texte
-    $Password = ConvertTo-SecureString -String $Password -AsPlainText -Force
-    $cred = New-Object -TypeName PSCredential -ArgumentList ($Username, $Password) #>
-
     New-AzVm -ResourceGroupName $RG_name -Location $Location_name -Name $vm_name -SecurityGroupName $NSG_name -VirtualNetworkName $VNet_name -SubnetName $subnet_name `
-    <# -Credential $cred #>
+    -ImageName "MicrosoftWindowsServer:WindowsServer:2016-Datacenter-Server-Core:latest" 
 
-    #Update le "Standard_B1s" à la création de la VM (y'a un autre moyen mais + long, faut se pencher dessus)
-    Stop-AzVM -ResourceGroupName $RG_name -Name $vm_name -Force
-    $vm = Get-AzVM -ResourceGroupName $RG_name -VMName $vm_name
-    $vm.HardwareProfile.VmSize = "Standard_B1s"
-    Update-AzVM -VM $vm -ResourceGroupName $RG_name
-    Start-AzVM -ResourceGroupName $RG_name  -Name $vm.name
+    #Pour WS2016 : ImageName = MicrosoftWindowsServer:WindowsServer:2016-Datacenter-Server-Core:latest
+    #Pour WS2019 : ImageName = MicrosoftWindowsServer:WindowsServer:2019-Datacenter-Core:latest
+
 }
